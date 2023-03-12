7bc8eaeace15f97646e707beeddb6f4e9fa01da6
Umut Sevdi <sevdiumut@gmail.com>
2023-03-12 23:48:32 +0300

    Added Makefile and compile_flags

--- tutorials-1/src/main.cpp
+++ tutorials-1/src/main.cpp
@@ -1,33 +1,7 @@
-/******************************************************************************
-
- * File: main.cpp
- *
- * Author: Umut Sevdi
- * Created: 03/12/23
- * Description:
- *     Bioinformatics Example 1 - Transcribe the following DNA to RNA, then
- use the genetic code to translate it to a sequence of amino acids.
-
-*****************************************************************************/
+#include "example_1.hpp"
 #include <algorithm>
 #include <cctype>
 #include <iostream>
-#include <string>
-
-/**
- * Returns a copy of the string in which all tabs, spaces or newline characters
- * are removed.
- */
-inline std::string erase_space(const std::string &str);
-/**
- * Transcribes the DNA sequence, in place. Returns the input.
- */
-inline std::string &transcribe_dna(std::string &str);
-/**
- * Replaces all Thymine(T) with Uracil(U), in place.
- * Returns the input.
- */
-inline std::string &thymine2uracil(std::string &str);
 
 int main(int argc, char *argv[]) {
   std::string s;
